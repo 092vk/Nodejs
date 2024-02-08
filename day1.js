cbb58eeb11154003d77d53724ef4fd680373e766
092vk <vk092kumar@gmail.com>
2024-02-08 23:40:14 +0530

    added day1 -day4

---
+++ day1.js
@@ -0,0 +1,4 @@
+/*
+nodejs is not a js library or a framework but a run time environment for js that allows us to run js outside of js and thus opens the door of creating webservers or do backend in js only  */
+
+
---
+++ day3.js
@@ -0,0 +1,10 @@
+// so together with nodejs , we get npm(node package manager )
+
+/*whenever we make a project - do npm init , this creates a package.json file ,
+ which keeps the record of all the libraries and dependices which we are using in our project */
+
+
+ console.log("hello js , this is my script");
+
+ //this file will run if you give the command "npm run myscript"
+// ***** so lets say we are making a project and instead of asking the devs to install all the dependiecs i can simply create a script and ask the devs to run it , that script will contain all the commands to install the dependies ******
