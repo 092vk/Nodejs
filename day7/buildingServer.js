80dd818d45ad36de7e274e1ade3b109d9f549be9
092vk <vk092kumar@gmail.com>
2024-03-03 00:38:16 +0530

    a

---
+++ day7/buildingServer.js
@@ -0,0 +1,35 @@
+// building our own server 
+//log the details of each request received with date,\
+
+
+import http from 'http';
+import fs from 'fs';
+
+const server = http.createServer((req,res)=>{
+    const log = `${Date.now()}: ${req.url} new req received \n`;
+    console.log("received a request");
+    fs.appendFile('./logs.txt',log,(err,content)=>{
+        if(err){
+            console.log("failed");
+            return;
+        }
+        switch(req.url){
+            case '/':
+                res.end("hello there user ");
+                break;
+            case '/about':
+                res.end("i am vivek kumar");
+                break;
+            default:
+                res.end("not found 404");
+        }
+        
+    });   
+})
+
+const port = 8080;
+
+server.listen(port,()=>{
+    console.log("server has started");
+})
+
---
+++ day8/url.js
@@ -0,0 +1,6 @@
+// url --> uniform resource locator 
+// https://www.piyushgarg.dev/
+
+// https:this is a protocol through which we are sending and receiving requests , other types are http , ws(web sockets), etc
+
+
