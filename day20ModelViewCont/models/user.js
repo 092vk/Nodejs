f2f7cc0a1fd4d732de0136ea2971280625363d23
092vk <vk092kumar@gmail.com>
2024-05-30 23:24:49 +0530

    added mongo files

---
+++ day20ModelViewCont/models/user.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+    firstName:{
+        type:String,
+        require:true
+    },
+    lastName:{
+        type:String
+    },
+    email:{
+        type:String,
+        require:true,
+        unique:true
+    },
+    jobTitle:{
+        type:String
+    },
+    gender:{
+        type:String,
+    }
+},{timestamps:true});
+
+
+const User= mongoose.model('user',userSchema);
+
+module.exports = User;
